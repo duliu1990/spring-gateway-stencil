a26f79808facf9ddbed0879b9a1a50c0a1687749
darren du <darren_l_du@manulife.com>
2023-08-08 21:25:00 +0800

    init

---
+++ src/test/java/com/proliu/SpringGatewayStencilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proliu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGatewayStencilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
